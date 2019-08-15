b53e6b23dbc7713898914c9742cea4345faef1d2
rusteam11 <rustemgal@gmail.com>
2019-08-15 12:04:29 +0300

    correct issues in readme,run.sh and calculate and cluster using insightface embeddings

--- process/run.sh
+++ process/run.sh
@@ -7,6 +7,7 @@ stage=$2
 
 /usr/local/bin/python3.7 /root/people_count/src/face_detector.py $query_date $stage
 /usr/local/bin/python3.7 /root/people_count/src/cluster_faces.py $query_date $stage
+/usr/local/bin/python3.7 /root/people_count/src/predict_age_gender.py $query_date $stage
 /usr/local/bin/python3.7 /root/people_count/src/retrieve_frames.py $query_date $stage
 /usr/local/bin/python3.7 /root/people_count/src/write_results.py $query_date $stage
 
